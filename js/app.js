12298666b0ef7648d355dc3865d974089359f9e4
carlosneto726 <carlosneto726@gmail.com>
2023-01-04 23:02:20 -0300

    começo da implementação do dark/light mode

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById("botao");
+const css = document.getElementById("appcss");
+const theme = window.localStorage.getItem("theme");
+
+if (theme === "Dark"){
+    css.href = "./css/app_dark.css";
+    btn.innerHTML = "Light"
+}else{
+    css.href = "./css/app.css";
+}
+
+
+function changeColorTheme(){
+    if(btn.innerHTML === "Dark"){
+        btn.innerHTML = "Light";
+        window.localStorage.setItem("theme", "Dark");
+        css.href = "./css/app_dark.css";
+
+    }else{
+        btn.innerHTML = "Dark";
+        window.localStorage.setItem("theme", "Light");
+        css.href = "./css/app.css";
+    } 
+}
+
+
+
